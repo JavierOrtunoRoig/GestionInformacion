2027becef337b9c2c81319aef976e640a7c260b9
David Castillo Berná <davidcastillo@uma.es>
2020-11-20 20:10:15 +0100

    Queries getPersonas, getAmigos, getDisponibles

---
+++ Queries.py
@@ -0,0 +1,81 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Patata_325",  ## Cambiar contraseña según BD personal
+    database="rastreocovid19"
+)
+
+
+def getPersonas():
+    listaPersonas = []
+
+    cursor = mydb.cursor()
+
+    cursor.execute("SELECT * FROM people")
+
+    result = cursor.fetchall()
+
+    for x in result:
+        listaPersonas.append(x)
+
+    return listaPersonas
+
+
+def getAmigos(id):
+    listaAmigos = []
+
+    cursor = mydb.cursor()
+
+    cursor.execute("SELECT ID2 FROM friends where ID1 = " + str(id))
+
+    result = cursor.fetchall()
+
+    selectStrFromPeople = "SELECT * FROM people where ID IN ("
+
+    for x in result:
+        selectStrFromPeople = selectStrFromPeople + str(x[0]) + ","
+
+    selectStrFromPeople = selectStrFromPeople[0:len(selectStrFromPeople) - 1] + ")"
+
+    cursor.execute(selectStrFromPeople)
+
+    result = cursor.fetchall()
+
+    for x in result:
+        listaAmigos.append(x)
+
+    return listaAmigos
+
+
+def getDisponibles(id):
+  listaDisponibles = []
+
+  cursor = mydb.cursor()
+
+  cursor.execute("SELECT ID2 FROM friends where ID1 = " + str(id))
+
+  result = cursor.fetchall()
+
+  selectStrFromPeople = "SELECT * FROM people where ID NOT IN (" + "'" + str(id) + "',"
+
+  for x in result:
+    selectStrFromPeople = selectStrFromPeople + str(x[0]) + ","
+
+  selectStrFromPeople = selectStrFromPeople[0:len(selectStrFromPeople) - 1] + ")"
+
+  cursor.execute(selectStrFromPeople)
+
+  result = cursor.fetchall()
+
+  for x in result:
+    listaDisponibles.append(x)
+
+  return listaDisponibles
+
+
+l = getDisponibles(1)
+
+for x in l:
+  print(x)
